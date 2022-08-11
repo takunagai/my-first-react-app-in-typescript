95840e78c38ce1e2c8eb3faa30c4b282e526c41b
takna <tak_na@icloud.com>
2022-08-12 08:17:44 +0900

    props で許可した属性以外が来るとエラーにする

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 
-// 関数コンポーネント。最終的に HTML として出力
-const App = (props: any) => { // コンポーネントから渡された属性を props にセット。ここで使用し、型指定も必要。とりあえず any 指定で消えた
-  console.log(props);
-  const { message } = props; // オブジェクトから、メッセージというメンバーだけ取り出す
+const App = ({message}: { message: string } ) => { // message 以外は許容しないように。実際は interface を使うことがほとんど
   return <div>{message}</div>;
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,25 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'; // 自作のモジュール(コンポーネント)
 
-(() => {
-    /**
-     * TypeScript で発生するエラーと、Intrinsic な属性の実体を確認
-     */
-    // // ../public/index.html の #root を <App /> で置き換えてレンダー
-    // ReactDOM.render(<App message="Hello, React!" key="1" />, document.getElementById('root'));
-    // // エラー：型 '{ message: string; }' を型 'IntrinsicAttributes(本来備わっている)' に割り当てることはできません。
-    // // エラーにならない Intrinsic な属性(特別扱い)： key="1"
-
-    // // key の実体を確認してみる
-    // type Foo = JSX.IntrinsicAttributes;
-    // // IntrinsicAttributes を右クリック > 定義に移動 で実体を見る
-    // // → React.Attributes を継承しているのが確認できる、さらに同様にしてその Attributes の実体を見る
-    // // → `key?: Key | null | undefined;` と確認できる → 登録されている！
-    // // `message?: string;` と追記すると、上記のエラーが消えることを確認 → Interface Attribute がキモ (このファイルを変更したらダメなので戻す)
-
-    // // ↑ のエラーはのちに解説
-})();
-
-
-// コンポーネントの使用先で props をセットすると、↑ のエラーは消える
+// ReactDOM.render(<App message="Hello, React!" name="Ham" />, document.getElementById('root')); // 許可していない属性はエラー
 ReactDOM.render(<App message="Hello, React!" />, document.getElementById('root'));
