90a79d399b343a31744395d22e0df63ccfbe9020
takna <tak_na@icloud.com>
2022-08-13 07:28:44 +0900

    useRef, useEffect (レンダリングされるごとに値が増える処理) 1

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,24 +1,34 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
+// useRef 追加、カーソルオン > クリックで型に移動し見る
+// `function useRef<T>(initialValue: T): MutableRefObject<T>;` 型引数を伴う関数型
+// 返り値の MutableRefObject にカーソルオン > 移動で確認すると
+// `interface MutableRefObject<T> {current: T;}`
 
 const Counter: React.FC<{}> = () => {
 
-    const initialValue: any = 0; // 前回の説明のため、意図的に any になってる
-    const [value, setValue] = useState<number>(initialValue); // 型引数の型制約、コールバック関数の prevState に影響する(A)
+    const initialValue: any = 0;
+    const [value, setValue] = useState<number>(initialValue);
 
-    // Counter コンポーネントが管理する value をアップデートする関数を定義
-    // setValue は、引数にコールバック関数を受け取ることもできる
     const increment = () => {
-        setValue(prevState => prevState + 1); // (A) prevState にカーソルオンすると、useState で指定した number 型
+        setValue(prevState => prevState + 1);
     };
     const decrement = () => {
         setValue(prevState => prevState - 1);
     };
 
+    /**
+     * 何回レンダリングしているかを計測する機能を実装していく
+     * (レンダリングされるごとに、値が増える)
+     */
+    const renderTimes = useRef(0); // renderTimes に 0 を保持させている
+    // ↑ の型定義で見た通り、値の取り出しは、current で取り出せる → (B)
+
     return (
         <div>
-            <div>value: {value}</div> {/*状態。ロード時レンダリングから、変化起きない*/}
+            <div>value: {value}</div>
             <button onClick={increment}>+1</button>
             <button onClick={decrement}>-1</button>
+            <div>This component was re-renderd {renderTimes.current} times.</div>{/*(B)*/}
         </div>
     );
 }
