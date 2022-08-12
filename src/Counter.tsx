8a4e1066de82bc1895167b9bd4704f13f24470a3
takna <tak_na@icloud.com>
2022-08-12 20:08:21 +0900

    useState の型引数で、受け取れる型を制限する

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react'; // useState もインポート
 
 // const Counter: React.FunctionComponent<{}> = () => { // コンポーネント関数に型を指定。空オブジェクトで良い
 const Counter: React.FC<{}> = () => { // 等価の処理、短く書ける TODO: React 18 の書き方に変更
-    const value = 0;
+
+    // const initialValue: any = 0; // any 型だと…
+    // const [value, setValue] = useState(initialValue); // value も any 型に。良くない。number 型に制約したい
+
+    const initialValue: any = 0; // 
+    const [value, setValue] = useState<number>(initialValue); // useState にカーソルオン > 定義に移動で型を見ると…
+    // function useState<S>(initialState: S | (() => S)): [S, Dispatch<SetStateAction<S>>]; // 型引数が設定できることを確認
+    //  ↑ useState の型引数に指定した型を受け取り、→ `useState<number>` で、value が any でなく number 型になる
+
+    // setValue にカーソルオンしてみると、すでに : React.Dispatch<React.SetStateAction<number>> と設定されてる
+    //   setValue の型は、React.Dispatch で型引数を持つ。その型引数の中の SetStateAction で型引数が指定されている
+    //   型は決まっていて、実装者が変更する余地はない。useState の型引数に与える型で自動で決まる
+    //   <number> を <string> に変えると、 : React.Dispatch<React.SetStateAction<string>> と自動で変更される
+
+    // React.Dispatch を分解して理解する
+    //   setValue にカーソルオン > 型定義へ移動(定義に移動ではない) し見てみる
+    //   `type Dispatch<A> = (value: A) => void;`
+    //   A にあたる SetStateAction は、その上の行に
+    //   `type SetStateAction<S> = S | ((prevState: S) => S);`
+    //    型引数で指定した型のデータ、もしくは、型引数として指定した型のデータを引数として、かつその型と同じ型のデータを返す関数のいずれかを型として持つ
+
     return (
         <div>
             <div>value: {value}</div> {/*状態。ロード時レンダリングから、変化起きない*/}
