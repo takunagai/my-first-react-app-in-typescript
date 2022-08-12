0613eaff99abc9653f42ba8913f4e64187eaf5d0
takna <tak_na@icloud.com>
2022-08-12 20:15:29 +0900

    setValue の型 React.Dispatch を分解して理解する

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -3,6 +3,13 @@ import React, { useState } from 'react'; // useState もインポート
 // const Counter: React.FunctionComponent<{}> = () => { // コンポーネント関数に型を指定。空オブジェクトで良い
 const Counter: React.FC<{}> = () => { // 等価の処理、短く書ける TODO: React 18 の書き方に変更
 
+    // setValue の説明：下の AA と BB を自前実装してみる
+    // type MySetStateAction<S> = S | ((prevState: S) => S);
+    type MySetStateAction = number | ((prevState: number) => number); // S を number と直書き
+    // type MyDispatch<A> = (value: A) => void;
+    // type MyDispatch = (value: MySetStateAction) => void; // setValue の型は、これそのもの
+    type MyDispatch = (value: number | ((prevState: number) => number)) => void; // MySetStateAction を埋め込んだ
+
     // const initialValue: any = 0; // any 型だと…
     // const [value, setValue] = useState(initialValue); // value も any 型に。良くない。number 型に制約したい
 
@@ -18,9 +25,9 @@ const Counter: React.FC<{}> = () => { // 等価の処理、短く書ける TODO:
 
     // React.Dispatch を分解して理解する
     //   setValue にカーソルオン > 型定義へ移動(定義に移動ではない) し見てみる
-    //   `type Dispatch<A> = (value: A) => void;`
+    //   `type Dispatch<A> = (value: A) => void;` // BB
     //   A にあたる SetStateAction は、その上の行に
-    //   `type SetStateAction<S> = S | ((prevState: S) => S);`
+    //   `type SetStateAction<S> = S | ((prevState: S) => S);` // AA
     //    型引数で指定した型のデータ、もしくは、型引数として指定した型のデータを引数として、かつその型と同じ型のデータを返す関数のいずれかを型として持つ
 
     return (
