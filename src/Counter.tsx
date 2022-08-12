9637821dc23672fffc7429706862108b80184331
takna <tak_na@icloud.com>
2022-08-12 21:00:29 +0900

    setValueの引数、コールバック関数で書く

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -3,14 +3,15 @@ import React, { useState } from 'react';
 const Counter: React.FC<{}> = () => {
 
     const initialValue: any = 0; // 前回の説明のため、意図的に any になってる
-    const [value, setValue] = useState<number>(initialValue);
+    const [value, setValue] = useState<number>(initialValue); // 型引数の型制約、コールバック関数の prevState に影響する(A)
 
     // Counter コンポーネントが管理する value をアップデートする関数を定義
+    // setValue は、引数にコールバック関数を受け取ることもできる
     const increment = () => {
-        setValue(value + 1); // value を更新
+        setValue(prevState => prevState + 1); // (A) prevState にカーソルオンすると、useState で指定した number 型
     };
     const decrement = () => {
-        setValue(value - 1);
+        setValue(prevState => prevState - 1);
     };
 
     return (
