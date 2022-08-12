1860449f1194ba32085289faf01e914f8d040592
takna <tak_na@icloud.com>
2022-08-12 17:20:09 +0900

    関数コンポーネント2 初級編

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 
-// interface で外に切り離す。再利用も可能に
 interface AppProps {
-  message: string;
+  message?: string; // name props がない場合のために、オプショナルに
 }
 
-const App = ({ message }: AppProps ) => { // interface で指定
+// React から提供された、コンポーネントという型を持つ関数を使う
+// const App = ({ message }: AppProps ) => {
+const App: React.FunctionComponent<AppProps> = ({ message }) => { // コメントアウトしている上の行なら、↓ の A の場合にエラーが出ない
   return <div>{message}</div>; // カーソルオンで string 型なのを確認
+};
+
+// デフォルト値を設定してみる
+App.defaultProps = {
+  message: 'Hello, defaultProps!',
+  // message: 123, // 型が異なるのでエラー(A)
+  // description: 'This is App component.' // 不要なデータが混入してエラー(A)
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'; // 自作のモジュール(コンポーネント)
 
-// ReactDOM.render(<App message={true} />, document.getElementById('root')); // 文字列しか許可しないのでエラー ok
-ReactDOM.render(<App message="Hello, React!" />, document.getElementById('root'));
+// name props を渡さない場合、デフォルト値が適用されるように
+// ReactDOM.render(<App message="Hello, React!" />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root')); // name props を渡さないので、オプショナルにする必要がある
