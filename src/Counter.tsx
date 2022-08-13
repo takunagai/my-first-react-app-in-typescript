cad6aa583e3d7997b090ec56c960fe7ed092cfda
takna <tak_na@icloud.com>
2022-08-13 09:21:20 +0900

    useRef 実践 1 - useRef の型の説明

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,27 +1,4 @@
 import React, { useEffect, useRef, useState } from 'react';
-// useRef 追加、カーソルオン > クリックで型に移動し見る
-// `function useRef<T>(initialValue: T): MutableRefObject<T>;` 型引数を伴う関数型
-// 返り値の MutableRefObject にカーソルオン > 移動で確認すると
-// `interface MutableRefObject<T> {current: T;}`
-
-// useEffect 追加、カーソルオン > クリックで型に移動し見る
-// `function useEffect(effect: EffectCallback, deps?: DependencyList): void;` 型引数なし、引数に effect, deps? 戻り値なし
-// 第１引数 effect の型 EffectCallback にカーソルオン > 移動で確認すると
-// `type EffectCallback = () => (void | Destructor);` 関数型、引数なし、返り値はなしか Destructor
-// Destructor にカーソルオン > 移動で確認すると
-// `type Destructor = () => void | { [UNDEFINED_VOID_ONLY]: never };`
-// 第２引数 deps の型 EffectCallback にカーソルオン > 移動で確認すると
-// `type DependencyList = ReadonlyArray<unknown>;`
-// interface の ReadonlyArray にカーソルオン > 移動で確認すると、膨大な定義。
-// → DependencyList は、型引数に与えた型で構成される readonly な配列
-// → useEffect の第２引数 deps には、readonly な配列をセットできることがわかる
-
-// // ReadonlyArray の説明コード
-// const array: Array<number> = [1, 2, 3];
-// const readonlyarray: ReadonlyArray<number> = [1, 2, 3]; // 読み取り専用の配列
-// array[0] = 11; // 上書きできる
-// readonlyarray[0] = 11; // エラー、上書きできない
-
 
 const Counter: React.FC<{}> = () => {
 
@@ -35,27 +12,34 @@ const Counter: React.FC<{}> = () => {
         setValue(prevState => prevState - 1);
     };
 
-    /**
-     * useRef で、何回レンダリングしているかを計測する機能を実装していく
-     * (= 再レンダリングごとに、値が増える)
-     * 
-     * UseEffect: コンポーネントがレンダーされたら必ず実行される処理を書く
-     */
-    const renderTimes = useRef<number>(0); // renderTimes に 0 を保持させている。型引数で型制約
-    // ↑ の型定義で見た通り、値の取り出しは、current で取り出せる → (B)
+    const renderTimes = useRef<number>(0);
 
     useEffect(() => {
-        // ブラウザのコンソールを確認
-        // +1/-1 ボタンをクリック(再レンダリング)するたびに、この命令が実行される！
         renderTimes.current = renderTimes.current + 1;
     });
+
+    // useRef、null で初期化したオブジェクトを作成
+    const ref = useRef<HTMLInputElement>(null); // B、input 要素の ref で受け取る型 <HTMLInputElement> を型引数にすることで、変数 ref の方が React.RefObject<HTMLInputElement> とできる
+    /* useRef にカーソルオンで型を確認 */
+    /* useRef<null>(initialValue: null): React.MutableRefObject<null>、型引数が null、A で RefObject<T> としたい */
 
     return (
         <div>
             <div>value: {value}</div>
             <button onClick={increment}>+1</button>
             <button onClick={decrement}>-1</button>
-            <div>This component was re-renderd {renderTimes.current} times.</div>{/*(B)*/}
+            <div>This component was re-renderd {renderTimes.current} times.</div>
+            <input ref={ref} type="text" /> {/* 型を一致させた {ref} */}
+            {/* ref にカーソルオンで確認 → React.ClassAttributes<HTMLInputElement>.ref?: React.LegacyRef<HTMLInputElement> | undefined → HTMLInputElement で初期化 → B */}
+            {/* ref にカーソルオン > 定義に移動で確認 */}
+            {/* `ref?: LegacyRef<T> | undefined;` */}
+            {/* LegacyRef にカーソルオン > 定義に移動で確認 */}
+            {/* `type LegacyRef<T> = string | Ref<T>;` */}
+            {/* 【注意】string も許容されてるが今は非推奨なので、Ref に設定できるのは Ref<T> だけ */}
+            {/* 1行上に Ref 型の定義があるので確認 */}
+            {/* `type Ref<T> = RefCallback<T> | RefObject<T> | null;`、 今回は RefObject<T> */}{/* A */}
+            {/* RefObject にカーソルオン > 定義に移動で確認 */}
+            {/* `interface RefObject<T> {readonly current: T | null;}` */}
         </div>
     );
 }
